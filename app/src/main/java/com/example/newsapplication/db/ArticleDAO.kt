ec7915717fb8b66c9df29a2628ef05b06cce87b7
Zeinab Abdien <suzanabdien15@gmail.com>
2023-06-20 17:18:05 +0300

    set room database

---
+++ app/src/main/java/com/example/newsapplication/db/ArticleDAO.kt
@@ -0,0 +1,18 @@
+package com.example.newsapplication.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.newsapplication.models.Article
+
+@Dao
+interface ArticleDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsertArticle(article: Article): Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
---
+++ app/src/main/java/com/example/newsapplication/db/ArticleDatabase.kt
@@ -0,0 +1,36 @@
+package com.example.newsapplication.db
+
+import android.content.Context
+import androidx.room.*
+import com.example.newsapplication.models.Article
+
+@Database(
+    entities = [Article::class],
+    version = 1
+)
+@TypeConverters(Converters::class)
+abstract class ArticleDatabase : RoomDatabase() {
+
+    abstract fun getArticleDao(): ArticleDAO
+
+    companion object {
+        @Volatile
+        private var instance: ArticleDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+
+            instance ?: createDatabase(context).also { instance = it }
+
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                ArticleDatabase::class.java,
+                "article_db.db"
+            ).build()
+
+    }
+
+}
