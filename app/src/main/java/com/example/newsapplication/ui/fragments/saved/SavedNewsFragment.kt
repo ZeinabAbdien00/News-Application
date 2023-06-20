d0c3e3455bfac29b4407b4afbc930177c3d7ae0f
Zeinab Abdien <suzanabdien15@gmail.com>
2023-06-20 17:22:33 +0300

    search news and check internet

---
+++ app/src/main/java/com/example/newsapplication/ui/fragments/saved/SavedNewsFragment.kt
@@ -0,0 +1,103 @@
+package com.example.newsapplication.ui.fragments.saved
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.newsapplication.R
+import com.example.newsapplication.adapters.NewsAdapter
+import com.example.newsapplication.databinding.FragmentSavedNewsBinding
+import com.example.newsapplication.ui.NewsActivity
+import com.example.newsapplication.ui.NewsViewModel
+import com.google.android.material.snackbar.Snackbar
+
+
+class SavedNewsFragment : Fragment() {
+
+    private lateinit var binding: FragmentSavedNewsBinding
+    private lateinit var viewModel: NewsViewModel
+    private lateinit var newsAdapter: NewsAdapter
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSavedNewsBinding.inflate(layoutInflater)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
+
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+            findNavController().navigate(
+                R.id.action_savedNewsFragment_to_articleFragment,
+                bundle
+            )
+        }
+
+        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val article = newsAdapter.differ.currentList[position]
+                viewModel.deleteArticle(article)
+                Snackbar.make(view, "Article Deleted !", Snackbar.LENGTH_LONG).apply {
+                    setAction("Undo") {
+                        viewModel.savedArticle(article)
+                    }
+                    show()
+                }
+
+            }
+
+        }
+
+        ItemTouchHelper(itemTouchHelperCallback).apply {
+            attachToRecyclerView(binding.rvSavedNews)
+        }
+
+        viewModel.getSavedNews().observe(viewLifecycleOwner, Observer { articles ->
+            if(viewModel.hasInternetConnection()){
+                newsAdapter.differ.submitList(articles)
+            }else{
+                Toast.makeText(activity, "No Internet Connection" , Toast.LENGTH_LONG).show()
+            }
+
+        })
+
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        binding.rvSavedNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+}
