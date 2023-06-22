b4df6603ae424ae14a8bbb008b6309d7008e636e
Zeinab Abdien <suzanabdien15@gmail.com>
2023-06-22 16:53:11 +0300

    add navigation back button

--- app/src/main/java/com/example/newsapplication/ui/NewsActivity.kt
+++ app/src/main/java/com/example/newsapplication/ui/NewsActivity.kt
@@ -2,9 +2,12 @@ package com.example.newsapplication.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.*
 import com.example.newsapplication.R
 import com.example.newsapplication.databinding.ActivityNewsBinding
 import com.example.newsapplication.db.ArticleDatabase
@@ -15,6 +18,8 @@ class NewsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewsBinding
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController : NavController
 
     private lateinit var repository: NewsRepository
     lateinit var viewModel: NewsViewModel
@@ -25,11 +30,32 @@ class NewsActivity : AppCompatActivity() {
         binding = ActivityNewsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(),
+            fallbackOnNavigateUpListener = ::onSupportNavigateUp
+        )
+        findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
+
+
+
+
+
+
+
+
         repository = NewsRepository(ArticleDatabase(this))
         viewModelProviderFactory = NewsViewModelProviderFactory(application, repository)
         viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
 
         setupBottomNavigation()
+        setupActionBar()
     }
 
     private fun setupBottomNavigation() {
@@ -37,5 +63,17 @@ class NewsActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
         bottomNavigationView = binding.bottomNavigationView
         NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.navController)
+    }
+
+    private fun setupActionBar(){
+        //navController = findNavController(R.id.newsNavHostFragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController , appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.newsNavHostFragment)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
